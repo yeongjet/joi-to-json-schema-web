54e9cd5fe79a696ee8bdc1825674b013b05687da
yeongjet <625436194@163.com>
2019-12-23 18:44:16 +0800

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,17 @@ import { AppService } from './app.service';
 import convert from '@yeongjet/joi-to-json-schema'
 import * as Joi from '@hapi/joi'
 
+const { alternatives, any, array, binary, boolean, link, number, string, symbol, object, date } = Joi.types();
+
+// 此处仅仅是引用解析字符串所使用的joi简写变量，否则这些变量编译后会被删除，导致调用eval方式时报Unexpected number之类的错误
+[alternatives, any, array, binary, boolean, link, number, string, symbol, object, date].toString().substr(0,0)
+
 @Controller('schema')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  getHello(@Body() body: { input: string }): { code: number, message: string, data?: Object } {
+  convertToJSONSchema(@Body() body: { input: string }): { code: number, message: string, data?: Object } {
     try {
       Joi.version // 此处要使用Joi这个变量，否则Joi不会引入
       const joiSchema = eval(body.input)
